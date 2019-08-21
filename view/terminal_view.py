ac3ca8c7262b86c3ad662c67d4fef88e4e6b7e50
Szymon Turek <szymko149@gmail.com>
2019-08-21 14:42:38 +0200

    Add print menu function

--- view/terminal_view.py
+++ view/terminal_view.py
@@ -71,8 +71,12 @@ def print_menu(title, list_options, exit_message):
     Returns:
         None: This function doesn't return anything it only prints to console.
     """
-
-    # your code
+    print("{}:".format(title))
+    
+    for i in range(len(list_options)):
+        print("({}) {}".format(i+1, list_options[i]))
+    print("(0) {}".format(exit_message))
+    
 
 
 def get_inputs(list_labels, title):
